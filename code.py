2f05baaf70ea61dc66728d8e7e8125bdab8c10e5
Jeil Jung <superlattice@users.noreply.github.com>
2021-01-29 13:09:41 +0900

    Create code.py

---
+++ code.py
@@ -0,0 +1,16 @@
+# Python file
+# Definition of a matrix using lists. 
+# Define a matrix the pedestrian way. 
+
+
+b = []
+# Review about continue and break
+for j in range(5):
+    a = []
+    for i in range(5):
+            a.append(i)
+            print('a ',a)
+        
+    b.append(a)
+    print('b ',b)
+    
